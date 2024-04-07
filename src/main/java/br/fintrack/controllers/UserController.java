e31ae533c06ca42f139491cd199f5c9481bd7baa
Joutos <joaodemeto123@gmail.com>
2024-04-06 21:59:51 -0300

    Adjust: Criação do CRUD de User + Exceção NotFound

---
+++ src/main/java/br/fintrack/controllers/UserController.java
@@ -0,0 +1,95 @@
+package br.fintrack.controllers;
+
+import br.fintrack.exception.ResourceNotFoundException;
+import br.fintrack.models.User;
+import br.fintrack.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  /*
+   * Lista todos os usuários
+   */
+  @GetMapping("/users")
+  public List<User> listUsers() {
+    return userRepository.findAll();
+  }
+
+  /*
+   * Lista um usuário específico pelo Id
+   */
+  @GetMapping("/users/{idUsuario}")
+  public User listUserById(@PathVariable(value = "idUsuario") short idUsuario) throws ResourceNotFoundException {
+    return userRepository.findById(idUsuario)
+        .orElseThrow(() -> new ResourceNotFoundException("Usuario não encontrado."));
+  }
+
+  /*
+   * Cria um novo usuário
+   */
+  @PostMapping("/users")
+  public User createUser(@Valid @RequestBody User user) {
+    try {
+      return userRepository.save(user);
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  /*
+   * Atualiza um usuário
+   */
+  @PutMapping("/users/{idUsuario}")
+  public ResponseEntity<User> updateUser(@PathVariable(value = "idUsuario") short idUsuario,
+      @Valid @RequestBody User userDetails) throws ResourceNotFoundException {
+    User user = userRepository.findById(idUsuario)
+        .orElseThrow(() -> new ResourceNotFoundException("Usuario não encontrado."));
+
+    if (userDetails.getEmail() != null) {
+      user.setEmail(userDetails.getEmail());
+    }
+    if (userDetails.getDataNascimento() != null) {
+      user.setDataNascimento(userDetails.getDataNascimento());
+    }
+    if (userDetails.getNome() != null) {
+      user.setNome(userDetails.getNome());
+    }
+
+    final User updatedUser = userRepository.save(user);
+    return ResponseEntity.ok(updatedUser);
+  }
+
+  /*
+   * Deleta um usuário
+   */
+  @DeleteMapping("/users/{idUsuario}")
+  public Map<String, Boolean> deleteUser(@PathVariable(value = "idUsuario") short idUsuario) {
+    try {
+      userRepository.deleteById(idUsuario);
+    } catch (Exception e) {
+      Map<String, Boolean> response = new HashMap<>();
+
+      response.put("deleted", Boolean.FALSE);
+      response.put("error", Boolean.TRUE);
+      return response;
+    }
+    Map<String, Boolean> response = new HashMap<>();
+    response.put("deleted", Boolean.TRUE);
+    response.put("error", Boolean.FALSE);
+    return response;
+  }
+}
--- src/main/java/br/fintrack/models/User.java
+++ src/main/java/br/fintrack/models/User.java
@@ -53,19 +53,19 @@ public class User {
         this.email = email;
     }
 
-    public String getnome() {
+    public String getNome() {
         return nome;
     }
 
-    public void setnome(String nome) {
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public Date getdataNascimento() {
+    public Date getDataNascimento() {
         return dataNascimento;
     }
 
-    public void setdataNascimento(Date dataNascimento) {
+    public void setDataNascimento(Date dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
 
@@ -73,7 +73,7 @@ public class User {
         return senha;
     }
 
-    public void setSenhate(String senha){
+    public void setSenha(String senha){
         this.senha = senha;
     }
 
